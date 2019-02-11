249464438e6f46f89f7a1da7153b237ee9ac8c4a
muge <muge.hasilci@hotmail.com>
2019-02-11 19:48:13 +0000

    default capacity defined

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -20,7 +20,7 @@ end
 describe '#dock' do
   it { is_expected.to respond_to(:dock).with(1).argument }
   it 'raises an error when docking station is full' do
-    20.times { subject.dock Bike.new }
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
     expect { subject.dock Bike.new }.to raise_error "Docking station is full"
   end
 end
