c410e500cddd366e485ca65d1b530a9c211c603a
muge <muge.hasilci@hotmail.com>
2019-02-12 11:58:44 +0000

    it releases working bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,13 +13,20 @@ describe DockingStation do
     end
   end
 
-
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
+
+    it { is_expected.to respond_to :release_bike }
+
+     it 'releases working bikes' do
+       subject.dock Bike.new
+       bike = subject.release_bike
+       expect(bike).to be_working
+     end
 
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
@@ -32,5 +39,6 @@ describe '#dock' do
     expect { subject.dock Bike.new }.to raise_error "Docking station is full"
   end
 end
+
 
 end
